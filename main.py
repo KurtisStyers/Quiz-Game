99481c9ab6d09d5a76ad4e4d28983d8a28e8f477
Kurtis Styers <kstyers86@gmail.com>
2022-07-23 11:51:27 -0700

    Create QuizBrain class with constructor and method for displaying questions

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for questions in question_data:
     question_bank.append(Question(questions["text"], questions["answer"]))
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
---
+++ quiz_brain.py
@@ -0,0 +1,9 @@
+class QuizBrain:
+    def __init__(self, questions):
+        self.question_number = 0
+        self.question_list = questions
+
+    def next_question(self):
+        question = self.question_list[self.question_number]
+        self.question_number += 1
+        answer = input(f"Q.{self.question_number}: {question.text} (True/False)?: ").lower()
